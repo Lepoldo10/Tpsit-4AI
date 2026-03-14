8d2a53cf7e323c4454ad6cfcb291b4d570913064
Lepoldo10 <leonardo.signorini@stu.istitutoturoldo.edu.it>
2026-03-14 12:38:00 +0100

    Leo

---
+++ 2.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int numeroIntero = 42;
+        double numeroDecimale = 3.1;
+        String testo = "Ciao, Java!";
+        System.out.println("Valore intero: " + numeroIntero);
+        System.out.println("Valore decimale: " + numeroDecimale);
+        System.out.println("Testo: " + testo);
+    }
+}
