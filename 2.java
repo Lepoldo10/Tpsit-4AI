b962be5b5ec0d6d2ef5ec8fbb143be2308ee24ca
Lepoldo10 <leonardo.signorini@stu.istitutoturoldo.edu.it>
2026-03-14 12:41:09 +0100

    leo

--- 2.java
+++ 2.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Dichiarazione e assegnazione delle variabili
-        int numeroIntero = 10;          // variabile intera
-        double numeroDecimale = 20.5;   // variabile decimale
-        String testo = "Hello, Java!";  // variabile Stringa
-        
-        // Stampa delle variabili
+
+        int numeroIntero = 10;        
+        double numeroDecimale = 20.5;   
+        String testo = "Hello, Java!";  
+
         System.out.println("Valore intero: " + numeroIntero);
         System.out.println("Valore decimale: " + numeroDecimale);
         System.out.println("Testo: " + testo);
